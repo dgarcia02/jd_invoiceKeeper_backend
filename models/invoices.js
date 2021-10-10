617f2ed9d2bcfebc619c5c1f56f6a219aab4fbc1
Dulce Garcia <dulcegarcia05.21@gmail.com>
2021-10-10 18:32:22 -0500

    created invoice model

---
+++ models/invoices.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+// Schema
+const invoiceSchema = new mongoose.Schema(
+    {
+        invoiceNumber: Number,
+        customer: {
+            name: String, 
+            from: String,
+            phone: Number
+        },
+        date: Date,
+        truckInfo: {
+            truckNumber: Number,
+            make: String,
+            engine: String
+        },
+        parts: {
+            qty: Number,
+            description: String,
+            price: Number,
+            amount: Number,
+            totalParts: Number
+        },
+        labor: {
+            itemNumber: Number,
+            description: String,
+            amount: Number,
+            totalLabor: Number
+        },
+        miscellaneous: {
+            items: {
+                machineShop: Number,
+                towService: Number,
+                roadService: Number,
+                computerProgram: Number,
+                diagnostic: Number,
+                shopMaterials: Number,
+                afterHours: Number   
+            },
+            totalMiscellaneous: Number
+        },
+        notes: String,
+        invoiceAmount: {
+            tax: Number,
+            grandTotal: Number
+        }
+    }
+)
+
+const Invoices = mongoose.model('Invoice', invoiceSchema);
+
+module.exports = Invoices;
