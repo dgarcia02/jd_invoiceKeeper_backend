46307bcc1857f8b445af3b7be74a3cb17e130468
Dulce Garcia <dulcegarcia05.21@gmail.com>
2021-10-11 16:13:20 -0500

    set up the get route in the invoice controller

--- controllers/invoices_controller.js
+++ controllers/invoices_controller.js
@@ -3,3 +3,23 @@ const router = express.Router()
 // grabbing the invoice model
 const Invoices = require('../models/invoices.js')
 
+// Routes
+// GET index
+router.get('/', (req, res) => {
+    Invoices.find({}, (error, foundInvoices) => {
+        res.json(foundInvoices)
+    })
+})
+
+
+
+// Seed Route
+router.get('/seed', (req, res) => {
+    Invoices.create(
+        [
+            {
+                
+            }
+        ]
+    )
+})
