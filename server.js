dcb27875bf22641e4b56ef2aa09bb18add211088
Dulce Garcia <dulcegarcia05.21@gmail.com>
2021-10-12 16:15:40 -0500

    set up server.js for the invoices controller and routes

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-// Dependencies //
+// ============== Dependencies ============== //
 const express = require('express')
-const mongoose = retuire('mongoose')
+const mongoose = require('mongoose')
 const cors = require('cors')
+
+
+// ============== Controllers ============== //
+const invoicesController = require('./controllers/invoicesController.js')
+
+
+// ============== Configuration ============== //
 const app = express()
-
-// Controllers //
-
-
-// Middleware //
+const db = mongoose.connection;
 
 
-// Database //
+// ============== Middleware ============== //
+app.use(express.json())
+app.use(cors())
+app.use('/invoices', invoicesController)
+
+
+// ============== PORT ============== //
+const PORT = process.env.PORT || 3003;
+
+
+// ============== Database ============== //
+// connect via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI , 
@@ -21,15 +35,20 @@ mongoose.connect(MONGODB_URI ,
     }
 )
 
+
+// ============== Routes ============== //
 app.get('/', (req, res) => {
     res.send('hello')
 })
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to Mongod...');
-})
 
-// Port //
+// ============== Connection Error/Success ============== //
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+
+// ============== Listener ============== //
 app.listen(3000, () => {
     console.log('listening on port ...', 3000);
 })
